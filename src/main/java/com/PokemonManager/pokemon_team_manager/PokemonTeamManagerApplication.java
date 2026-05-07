cba1d42d377871bf90bb758af6b6320cd2a03e58
flavius.minoiu <minoiu16m@gmail.com>
2026-05-07 11:32:56 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/PokemonManager/pokemon_team_manager/PokemonTeamManagerApplication.java
@@ -0,0 +1,13 @@
+package com.PokemonManager.pokemon_team_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTeamManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonTeamManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PokemonManager/pokemon_team_manager/PokemonTeamManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PokemonManager.pokemon_team_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTeamManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
